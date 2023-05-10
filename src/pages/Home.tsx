5def74a55248c59411ccb51fbfc89f332278035b
Clarence Manuel <rencedm112@gmail.com>
2023-05-10 13:04:42 +0800

    Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -65,7 +65,7 @@ export const HomePage: React.FC = () => {
   ];
 
   const statAdditionalClass =
-    "border-on-surface-variant/[25%] border-b sm:border-b-0 sm:border-r last:border-b-0 sm:even:border-r-0 xl:[&:nth-child(2)]:border-r xl:last:border-r-0 py-16";
+    "border-on-surface-variant/[25%] border-b sm:border-b-0 sm:border-r last:border-b-0 sm:even:border-r-0 xl:[&:nth-child(2)]:border-r xl:last:border-r-0 p-6";
 
   const drawer = (
     <Drawer>
@@ -175,7 +175,7 @@ export const HomePage: React.FC = () => {
           </div>
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 2xl:grid-cols-3">
             {/* Stats */}
-            <Card className="col-span-1 grid !p-0 sm:grid-cols-2 md:col-span-2 xl:grid-cols-4 2xl:col-span-3">
+            <Card className="col-span-1 grid !p-0 sm:grid-cols-2 md:col-span-2 xl:grid-cols-3 2xl:col-span-3">
               {/* Total Revenue */}
               <div
                 className={c(
@@ -183,176 +183,64 @@ export const HomePage: React.FC = () => {
                   statAdditionalClass,
                 )}
               >
-                <span className="flex flex-row items-center gap-2 align-middle text-label-large text-on-surface-variant">
-                  <Icon className="text-[42px] text-success">
+                <span className="flex place-self-start flex-row items-center gap-2 align-middle text-label-large text-on-surface-variant">
+                  <Icon
+                    className="text-[42px] text-success"
+                    filled
+                  >
                     monetization_on
                   </Icon>
                   Total Revenue
                 </span>
                 <span className="text-headline-large font-medium">
-                  $ 140.00
+                  <span className="text-on-surface-variant/50">$</span>
+                  140.00
                 </span>
-                <span className="rounded-full bg-success-container p-1 px-4 text-label-small text-on-success-container">
+                <span className="rounded-full place-self-end bg-success-container p-1 px-4 text-label-small text-on-success-container">
                   +$100
                   <Icon className="text-[16px]">arrow_upward</Icon>
                 </span>
               </div>
-
-              {/* Personal Volume */}
+              {/* Paid Commission */}
               <div
                 className={c(
                   "flex flex-col items-center gap-4",
                   statAdditionalClass,
                 )}
               >
-                <span className="flex flex-row items-center gap-2 align-middle text-label-large text-on-surface-variant">
-                  <Icon className="text-[42px] text-primary">stars</Icon>
-                  Personal Volume
-                </span>
-                <span className="text-headline-large font-medium">20 PV</span>
-                <span className="rounded-full bg-success-container p-1 px-4 text-label-small text-on-success-container">
-                  +20 PV
-                  <Icon className="text-[16px]">arrow_upward</Icon>
-                </span>
-              </div>
-
-              {/* New Members */}
-              <div
-                className={c(
-                  "flex flex-col items-center gap-4",
-                  statAdditionalClass,
-                )}
-              >
-                <span className="flex flex-row items-center gap-2 align-middle text-label-large text-on-surface-variant">
-                  <Icon className="text-[42px] text-orange">
-                    account_circle
+                <span className="flex place-self-start flex-row items-center gap-2 align-middle text-label-large text-on-surface-variant">
+                  <Icon
+                    className="text-[42px] text-success"
+                    filled
+                  >
+                    currency_exchange
                   </Icon>
-                  New Members
+                  Paid Commissions
                 </span>
-                <span className="text-headline-large font-medium">2</span>
-                <span className="rounded-full bg-success-container p-1 px-4 text-label-small text-on-success-container">
-                  +2 Members
+                <span className="text-headline-large font-medium">
+                  <span className="text-on-surface-variant/50">$</span>
+                  140.00
+                </span>
+                <span className="rounded-full place-self-end bg-success-container p-1 px-4 text-label-small text-on-success-container">
+                  +$100
                   <Icon className="text-[16px]">arrow_upward</Icon>
                 </span>
               </div>
-
-              {/* Account Status */}
+              {/*  */}
               <div
                 className={c(
                   "flex flex-col items-center gap-4",
                   statAdditionalClass,
                 )}
               >
-                <span className="flex flex-row items-center gap-2 align-middle text-label-large text-on-surface-variant">
-                  <Icon className="text-[42px] text-red">favorite</Icon>
-                  Account Status
+                <span className="flex place-self-start flex-row items-center gap-2 align-middle text-label-large text-on-surface-variant">
+                  <Icon className="text-[42px] text-primary">credit_card</Icon>
+                  Current Balance
                 </span>
-                <span className="text-headline-large font-medium opacity-disabled-content">
-                  O Day
+                <span className="text-headline-large font-medium">
+                  <span className="text-on-surface-variant/50">$</span>
+                  140.00
                 </span>
-                <div className="flex flex-col items-center gap-2">
-                  <span className="text-title-large text-error">Inactive</span>
-                  <span className="text-label-medium text-on-surface-variant">
-                    Check out our Hermon Monthly Plan
-                  </span>
-                </div>
-              </div>
-            </Card>
-
-            {/* Weekly Cycles */}
-            <Card className="col-span-1 flex flex-col gap-8 md:col-span-2">
-              <div className="flex flex-row items-end">
-                <h2 className="text-headline-large font-medium">
-                  Weekly Cycles
-                </h2>
-                <div className="flex-grow"></div>
-                <img
-                  src={calendar}
-                  alt="graph"
-                />
-              </div>
-
-              <div className="flex flex-row gap-4">
-                <select className="w-40 rounded-extra-large bg-surface-variant px-6 py-4">
-                  <option>Year</option>
-                </select>
-                <select className="w-40 rounded-extra-large bg-surface-variant px-6 py-4">
-                  <option>Month</option>
-                </select>
-              </div>
-
-              <div className="overflow-y-scroll">
-                <div className="flex min-w-max flex-row justify-around gap-4 pb-4">
-                  <div className="flex flex-col items-center gap-2">
-                    <Cycle />
-                    <p className="text-label-large">Week 1</p>
-                  </div>
-                  <div className="flex flex-col items-center gap-2">
-                    <Cycle selected={true} />
-                    <p className="text-label-large">Week 2</p>
-                  </div>
-                  <div className="flex flex-col items-center gap-2">
-                    <Cycle />
-                    <p className="text-label-large">Week 3</p>
-                  </div>
-                  <div className="flex flex-col items-center gap-2">
-                    <Cycle />
-                    <p className="text-label-large">Week 4</p>
-                  </div>
-                </div>
-              </div>
-            </Card>
-
-            {/* Weekly Profit */}
-            <Card className="col-span-1 flex flex-col gap-12">
-              <div className="flex flex-row items-end">
-                <h2 className="text-headline-large font-medium">
-                  Weekly Profit
-                </h2>
-                <div className="flex-grow"></div>
-                <img
-                  src={graph}
-                  alt="graph"
-                />
-              </div>
-
-              <table className="w-full table-auto">
-                <thead>
-                  <tr>
-                    <th className="pb-6 text-start">Bonus</th>
-                    <th className="pb-6 text-start">Profit</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th className="text-start font-medium">Fast Track Bonus</th>
-                    <td>$250</td>
-                  </tr>
-                  <tr>
-                    <th className="text-start font-medium">Cycle Bonus</th>
-                    <td>$62.50</td>
-                  </tr>
-                  <tr>
-                    <th className="text-start font-medium">Good Life Bonus</th>
-                    <td>$100</td>
-                  </tr>
-                  <tr>
-                    <th className="text-start font-medium">
-                      Leadership Legacy Bonus
-                    </th>
-                    <td>$40</td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="flex flex-col gap-4">
-                <p className="font-medium">Est. Week Total</p>
-                <p className="text-center text-display-large font-medium text-success">
-                  $442.5
-                </p>
-                <p className="text-center text-on-surface-variant">
-                  All profit will be transfered to your Hermon Wallet
-                  automatically.
-                </p>
               </div>
             </Card>
 
